d1978c45f1d5c4ef3cf78beb3676e08e14456cc9
przemyslaw.pazdziorek <p.pazdziorek@gmail.com>
2019-03-07 19:47:28 +0100

    some changes

--- src/main/java/com/pazdziorek/myProject/MyProjectApplication.java
+++ src/main/java/com/pazdziorek/myProject/MyProjectApplication.java
@@ -2,32 +2,34 @@ package com.pazdziorek.myProject;
 
 import com.pazdziorek.myProject.dataModel.FinancialData;
 import com.pazdziorek.myProject.domain.dataModel.DataRepository;
+import com.pazdziorek.myProject.repository.suppliers.RepositorySupplier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import javax.xml.crypto.Data;
-import java.util.Date;
-
 @SpringBootApplication
 @ComponentScan({"com.pazdziorek"})
 public class MyProjectApplication implements CommandLineRunner {
 
-	@Autowired
     DataRepository dataRepository;
-    @Autowired
     FinancialData financialData;
 
-	public static void main(String[] args) {
+    public MyProjectApplication(DataRepository dataRepository, FinancialData financialData) {
+        this.dataRepository = dataRepository;
+        this.financialData = financialData;
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(MyProjectApplication.class, args);
 	}
 
     @Override
     public void run(String... args) throws Exception {
+        RepositorySupplier repositorySupplier = new RepositorySupplier(dataRepository);
         Long id = 1L;
         financialData = new FinancialData(id, "Basic category","21 sep 2018");
-        dataRepository.save(financialData);
+        repositorySupplier.addNewEntry(financialData);
     }
 }
